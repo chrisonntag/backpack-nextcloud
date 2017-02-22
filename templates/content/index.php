f5a1538b4af740d6dd6fd1aece6d39ca1d91c931
chrisonntag <sonntagc@fim.uni-passau.de>
2017-02-22 10:37:26 +0100

    Add text inputs and adjust styles

--- templates/content/index.php
+++ templates/content/index.php
@@ -1,10 +1,24 @@
 <script id="content-tpl" type="text/x-handlebars-template">
     {{#if link}}
-        <div class="input"><textarea>{{ link.link }}</textarea></div>
+        <div class="input">
+        	<label for="title_field">Titel: </label>
+        	<input id="title_field" type="text" value="{{ link.title }}"></input>
+        </div>
+        <div class="input">
+        	<label for="link_field">Link: </label>
+        	<input id="link_field" type="text" value="{{ link.link }}"></input>
+        </div>
         <div class="save"><button><?php p($l->t('Save')); ?></button></div>
     {{else}}
-        <div class="input"><textarea disabled></textarea></div>
-        <div class="save"><button disabled><?php p($l->t('Save')); ?></button></div>
+        <div class="input">
+        	<label for="title_field">Titel: </label>
+        	<input id="title_field" type="text" disabled></input>
+        </div>
+        <div class="input">
+        	<label for="link_field">Link: </label>
+        	<input id="link_field" type="text" disabled></input>
+        </div>
+        <div class="save"><button disabled></button></div>
     {{/if}}
 </script>
 <div id="editor"></div>
